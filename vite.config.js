45dd3345c64e11706f2eecd97f7bb96aaf110fed
cinthiya <cinthiyamurugan@gmail.com>
2025-07-20 12:48:14 +0530

    6th commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Only apply base path when building for production
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/SOS-WOMEN/' : '/', // ← fix here
-}))
+  base: command === '/SOS-WOMEN/' // 👈 dynamic base
+}));
